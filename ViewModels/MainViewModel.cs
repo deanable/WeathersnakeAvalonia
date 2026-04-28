7e6133f52544ecdd2153b31c6ccdba9934bbd081
deanable <krugerable@gmail.com>
2026-04-28 14:38:25 +0200

    Fix macOS dock icon: copy .icns to Resources; update to v1.0.23

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -444,7 +444,7 @@ public partial class MainViewModel : ObservableObject
             IsAntialias = true,
             Typeface = SkiaSharp.SKTypeface.FromFamilyName("Arial")
         };
-        var watermarkText = "Weather Juice v1.0.22";
+        var watermarkText = "Weather Juice v1.0.23";
         canvas.DrawText(watermarkText, margin, height - 10, watermarkPaint);
 
         var logoUri = new Uri("avares://WeathersnakeAvalonia/Assets/logo.png");
